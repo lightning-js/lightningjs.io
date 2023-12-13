88ce530e8924968dfce9965e01105bb7c66a2e6e
jfboeve <jeffrey_boeve@comcast.com>
2023-12-13 10:19:38 +0100

    update config

--- site/.vitepress/config.mjs
+++ site/.vitepress/config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base: "./",
+  base: "/lightningjs.io/",
   title: "Lightningjs Site",
   description: "Lightningjs Nexus of Information",
   outDir: "../public",
