fe1c1a74e87716d196dec7b64f05548026c892b0
yve28 <48488590+yve28@users.noreply.github.com>
2023-12-20 13:19:51 +0100

    kleine aanpassing
    - logo kleiner gemaakt
    - bg-color - dorpdown menu - sm

--- site/.vitepress/theme/components/home/LegacyBlock.vue
+++ site/.vitepress/theme/components/home/LegacyBlock.vue
@@ -8,18 +8,17 @@ export default {
 </script>
 
 <template>
-    <section class="pt-14 md:pt-28 grid grid-cols-1 justify-center items-center">
+    <section class="pt-14 md:pt-28 grid grid-cols-1 justify-center items-center ">
         <SectionHeader :title="legacy.title" :description="legacy.description"/>
         <Popover class="relative mx-auto w-11/12 grid grid-cols-1 rounded-lg border-[1px] mt-8 md:hidden justify-center items-center">
             <PopoverButton class="w-full h-12 rounded-md border-[1px]">Lightning 2 Ecosystem</PopoverButton>
-            <PopoverPanel class="z-10 w-full absolute gap-2 grid grid-cols-2 top-14 rounded-lg p-4 bg-slate-900">
+            <PopoverPanel class="z-10 w-full absolute gap-2 grid grid-cols-2 top-14 rounded-lg p-4 drop-shadow-lg bg-slate-100 dark:bg-slate-900">
                 <a v-for="link in legacy.links" :href="link.url" target="_blank" 
                 class="border-[1px] h-12 rounded-md flex items-center justify-center"> 
                     <span>{{ link.title }}</span>
                 </a>
             </PopoverPanel>
         </Popover>
-
         <div class="hidden pt-12 md:grid gap-6 md:grid-cols-3">
             <a v-for="link in legacy.links" :href="link.url" target="_blank" 
             class="border-[1px] h-12 rounded-md flex items-center justify-center"> 
