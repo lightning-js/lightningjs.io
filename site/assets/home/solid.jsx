26f1baf1ed8b0a567222a7b4d0fd2a19e89cf78b
jfboeve <jeffrey_boeve@comcast.com>
2023-12-20 15:29:13 +0100

    formate sample jsx code

--- site/assets/home/solid.jsx
+++ site/assets/home/solid.jsx
@@ -1,11 +1,11 @@
 const App = () => {
-    const [title, setTitle] = createSignal('SolidJS')
-    const onEnter = () => {
-      setTitle('Lightning Renderer works with SolidJS')
-    }
-    return (
-      <View onEnter={onEnter}>
-        <Text>{title()}</Text>
-      </View>
-    )
+  const [title, setTitle] = createSignal('SolidJS')
+  const onEnter = () => {
+    setTitle('Lightning Renderer works with SolidJS')
   }
+  return (
+    <View onEnter={onEnter}>
+      <Text>{title()}</Text>
+    </View>
+  )
+}
