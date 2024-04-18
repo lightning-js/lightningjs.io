1f142cfaf514f442b7e19de07058640c6f477d68
jfboeve <jeffrey_boeve@comcast.com>
2024-04-18 12:36:18 +0200

    fix blits link HeroDislay

--- site/.vitepress/theme/components/home/HeroDisplay.vue
+++ site/.vitepress/theme/components/home/HeroDisplay.vue
@@ -19,7 +19,7 @@ import LinkButton from './LinkButton.vue';
             </h1>
             <h1 class="hidden dark:block text-4xl font-bold tracking-tight sm:text-7xl from-blue-200 via-purple-700 to-blue-600 bg-gradient-to-r bg-clip-text text-transparent animate-fade-up">More Performance, Less Code</h1>
             <p class="mt-6 text-lg leading-8 text-black dark:text-gray-200">Lightweight, open-source framework for developing hyper fast animated applications</p>
-            <LinkButton class="mt-10" href="https://lightningjs.io/v3-docs/blits/intro.html" label="Beta"/>
+            <LinkButton class="mt-10" href="https://lightningjs.io/v3-docs/blits/getting_started/intro.html" label="Beta"/>
           </div>
         </div>
       </div>
