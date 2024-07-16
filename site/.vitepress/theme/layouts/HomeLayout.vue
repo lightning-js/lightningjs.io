c6f456f4ed135fa2f5e5a830af72bd038cf44fd1
yve28 <48488590+yve28@users.noreply.github.com>
2024-07-16 12:44:36 +0200

    Announcement
    Small typo correct

--- site/.vitepress/theme/layouts/HomeLayout.vue
+++ site/.vitepress/theme/layouts/HomeLayout.vue
@@ -7,7 +7,8 @@ import Footer from '../components/home/Footer.vue'
 import JoinDiscord from '../components/home/JoinDiscord.vue'
 import LegacyBlock from '../components/home/LegacyBlock.vue'
 import BlitsContent from '../components/home/BlitsContent.vue'
-import AnnoumentBar  from '../components/home/AnnoumentBar.vue'
+import AnnouncementBar  from '../components/home/AnnouncementBar.vue'
+
 
 </script>
 <template>
@@ -16,7 +17,7 @@ import AnnoumentBar  from '../components/home/AnnoumentBar.vue'
     <HeroDisplay class="mt-6"/>
     <LightingjsInfo class="mt-24"/>
     <BlitsContent/>
-    <AnnoumentBar/>
+    <AnnouncementBar/>
     <NewFeature/>
     <Quote/>
     <LegacyBlock/>
